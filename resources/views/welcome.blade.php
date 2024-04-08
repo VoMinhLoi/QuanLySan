9f6638b1d6f24d883843f4af0776c756bf28a06d
VoMinhLoi <minhloi1131130@gmail.com>
2024-04-08 12:29:53 +0700

    Modify title Home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Sân đại học Thể Dục Thể Thao Đà Nẵng</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+        <link rel="icon" href="./assets/img/Logo-Truong-Dai-hoc-The-duc-The-thao-Da-Nang.png" type="image/icon type">
         {{-- Grid system css --}}
         <style>
             .grid {
