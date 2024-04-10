0a8887aeaddfcf85ba27eac2d6b1ab5635fe6a47
VoMinhLoi <minhloi1131130@gmail.com>
2024-04-10 10:32:29 +0700

    Reset layout of file grid system.

---
+++ resources/views/Pages/book.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('Components.grid_system')
+</head>
+<body>
+    <div class="grid">
+        @include('Components.header')
+        @include('Book.container')
+        @include('Components.footer')
+    </div>
+</body>
+</html>
