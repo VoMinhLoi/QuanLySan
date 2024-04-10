e430d1d696f27c21e7140139af4cead5fca41c80
VoMinhLoi <minhloi1131130@gmail.com>
2024-04-10 20:38:59 +0700

    Public

--- resources/views/Components/variable.blade.php
+++ resources/views/Components/variable.blade.php
@@ -10,3 +10,7 @@
         display: block !important;
     }
 </style>
+<script>
+    var $ = document.querySelector.bind(document);
+    var $$ = document.querySelectorAll.bind(document);
+</script>
